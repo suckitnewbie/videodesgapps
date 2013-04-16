bf8ec3a15824141b98ed0666b5a02a443473d5b7
Alda.Nevrela <Alda.Nevrela@gmail.com>
2013-04-16 14:43:33 +0000

    Initial import of NetBeans project

---
+++ VideoDesGApps/src/main/java/cz/muni/fi/pb138/videodesgapps/App.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pb138.videodesgapps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("I'm VideoDesGApps Application");
+    }
+}
