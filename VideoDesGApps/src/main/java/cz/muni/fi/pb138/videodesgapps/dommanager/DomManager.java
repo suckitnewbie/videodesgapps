b6b19494edb3cdc118fc3ff982fa9586b9697e08
jakublipcak <jakublipcak@190935cd-3de0-da7d-7070-a36f7bc9214a>
2013-05-31 13:26:01 +0000

    DomManager interface

---
+++ VideoDesGApps/src/main/java/cz/muni/fi/pb138/videodesgapps/dommanager/DomManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.videodesgapps.dommanager;
+
+import java.util.List;
+
+/**
+ *
+ * @author Jakub
+ */
+public interface DomManager {
+    void addMediaType(String name, List<String> attributes);
+    void deleteMediaType(String name);
+    void addRecord(String media, List<String> attributes);
+    void deleteRecord(String media, int id);
+    void editRecord(String media, int id, List<String> attributes);
+    List<Integer> searchRecord(String searchValue);
+}
---
+++ VideoDesGApps/src/main/java/cz/muni/fi/pb138/videodesgapps/dommanager/DomManagerImpl.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.videodesgapps.dommanager;
+
+import java.util.List;
+
+/**
+ *
+ * @author Jakub & Erik
+ */
+public class DomManagerImpl implements DomManager{
+
+    public void addMediaType(String name, List<String> attributes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void deleteMediaType(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void addRecord(String media, List<String> attributes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void deleteRecord(String media, int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void editRecord(String media, int id, List<String> attributes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<Integer> searchRecord(String searchValue) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
