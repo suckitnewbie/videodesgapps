14cec11cdfc5e6c1795a4abbf2da390a46b37f82
jakublipcak <jakublipcak@190935cd-3de0-da7d-7070-a36f7bc9214a>
2013-06-09 22:41:42 +0000

    Table Model

---
+++ VideoDesGApps/src/main/java/cz/muni/fi/pb138/videodesgapps/gui/components/OdfTableModel.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.videodesgapps.gui.components;
+
+import cz.muni.fi.pb138.videodesgapps.dommanager.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Jakub
+ */
+public class OdfTableModel extends AbstractTableModel {
+
+    private MediaType mediaType = new MediaType();
+
+    public OdfTableModel() {
+        
+    }
+
+    @Override
+    public int getRowCount() {
+        return mediaType.getRecords().size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return mediaType.getAttributes().size();
+    }
+
+    public void setMediaType(MediaType mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        ArrayList<String> record = (ArrayList<String>) mediaType.getRecords().get(rowIndex);
+        
+            if (columnIndex < getColumnCount()) return record.get(columnIndex).toString();
+            else throw new IllegalArgumentException("WRONG");
+    }
+    
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        
+            return mediaType.getAttributes().get(columnIndex).toString();
+        }
+    }
